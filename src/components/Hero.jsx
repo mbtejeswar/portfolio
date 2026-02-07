7b02f37edf505033f05eaee9e70843ce5389da08
Tejeswar Reddy M B <tejeswarreddymb@Tejeswars-Mac-mini.local>
2026-02-07 21:08:00 +0530

    update name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -93,7 +93,7 @@ const Hero = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.5 }}
           >
-            Tejeswar Reddy
+            M B Tejeswar Reddy
           </motion.h1>
 
           <motion.h2
